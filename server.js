9686320a4c3153345d3a9988a505a3c08398c846
Ubuntu <ubuntu@ip-10-36-154-157.eu-west-1.compute.internal>
2013-07-08 20:54:44 +0000

    back to server.js

--- server.js
+++ server.js
@@ -2,9 +2,7 @@
 var http = require('http');
 
 http.createServer(function(request,response){
-    response.writeHead(200, {"Content-Type": "text/plain"});
     response.send("Hello World");
-    response.end();
 }).listen(5000);
 
 console.log("done");
