f128cb24e71c1dd3929febbc7d8d7c4db02a8e34
Ubuntu <ubuntu@ip-10-36-154-157.eu-west-1.compute.internal>
2013-07-08 20:34:51 +0000

    change port

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ http.createServer(function(request,response){
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
-}).listen(8888);
+}).listen(5000);
 
+console.log("done");
