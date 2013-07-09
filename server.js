933ba01be2f7e15cbe7da2cec3b9354463b089bd
Ubuntu <ubuntu@ip-10-37-147-56.eu-west-1.compute.internal>
2013-07-09 07:49:26 +0000

    update port as in web.js

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 
 var http = require('http');
 
+var port = process.env.PORT || 5000;
+
 http.createServer(function(request,response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
-}).listen(5000);
+}).listen(port);
 
 
 console.log("done");
