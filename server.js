3dc3727d4987ec23e80109da2fe626f440b97f34
Ubuntu <ubuntu@ip-10-37-147-56.eu-west-1.compute.internal>
2013-07-09 07:34:58 +0000

    copied paste

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 
+
 var http = require('http');
 
-http.createServer(function(request,response){
-    response.write("Hello World");
-}).listen(5000);
+http.createServer(function(request,response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8888);
+
 
 console.log("done");
