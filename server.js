3622475a329ef3f0dd9ef4315f1350b916a52b28
Ubuntu <ubuntu@ip-10-48-45-105.eu-west-1.compute.internal>
2013-07-15 15:02:19 +0000

    added index

--- server.js
+++ server.js
@@ -3,13 +3,19 @@
 var http = require('http');
 
 // there is something with Heroku and ports...
-var port = process.env.PORT || 5000;
 
-http.createServer(function(request,response) {
-  console.log("Request received."); 
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(port);
 
-console.log("Server has started");
+function start(){
+  var port = process.env.PORT || 5000;
+  http.createServer(function(request,response) {
+    console.log("Request received."); 
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }).listen(port);
+  }
+  console.log("Server has started");
+}
+
+exports.start = start;
+
