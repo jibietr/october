913a8b645175b15bd8c2af609131c9250a6b3256
Ubuntu <ubuntu@ip-10-48-45-105.eu-west-1.compute.internal>
2013-07-15 15:04:45 +0000

    correct server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var http = require('http');
 // there is something with Heroku and ports...
 
 
-function start(){
+function start() {
   var port = process.env.PORT || 5000;
   http.createServer(function(request,response) {
     console.log("Request received."); 
@@ -13,7 +13,7 @@ function start(){
     response.write("Hello World");
     response.end();
   }).listen(port);
-  }
+ 
   console.log("Server has started");
 }
 
