301f83e0a17da607af63d1368ea0af988cc163bb
Ubuntu <ubuntu@ip-10-48-45-105.eu-west-1.compute.internal>
2013-07-16 09:02:17 +0000

    add request listeners

--- server.js
+++ server.js
@@ -8,12 +8,23 @@ var url = require('url');
 function start(route, handle) {
   var port = process.env.PORT || 5000;
   http.createServer(function(request,response) {
+    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request received for"+ pathname + " received." ); 
-    //route request    
-    route(handle, pathname, response);
+   
+    request.setEncoding("utf8");
+   
+    request.addListener("data", function(postDataChunk){
+      postData += postDataChunk;
+      console.log("Received POST data chunk '" +
+       postDataChunk + "'.");
+    });
+    
+    request.addListener("end", function(){
+      route(handle, pathname, response);
+    });
+
   }).listen(port);
- 
   console.log("Server has started");
 }
 
