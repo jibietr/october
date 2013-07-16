5ad0d7ad1e38eef7958387ef5416049c2f8162d5
Ubuntu <ubuntu@ip-10-48-45-105.eu-west-1.compute.internal>
2013-07-16 08:49:56 +0000

    add submit button

--- requestHandlers.js
+++ requestHandlers.js
@@ -13,6 +13,7 @@ function start(response) {
    '<body>'+
    '<form action="/upload" method="post">' +
    '<textarea name="text" rows="20" cols="60"></textarea>' +
+   '<input type="submit" value="Submit text" />' +
    '</form>' +
    '</body>' +
    '</html>';
