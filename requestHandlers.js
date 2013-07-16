100652d4290fa29b6768bfc108c0ef4252b306ba
Ubuntu <ubuntu@ip-10-48-45-105.eu-west-1.compute.internal>
2013-07-16 08:47:29 +0000

    modify start function to show html instead of plain text

--- requestHandlers.js
+++ requestHandlers.js
@@ -17,7 +17,7 @@ function start(response) {
    '</body>' +
    '</html>';
 
-  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.writeHead(200, {"Content-Type": "text/html"});
   response.write(body);
   response.end();
 }
