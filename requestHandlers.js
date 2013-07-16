2fcdaf9ba4afecadc4d9fadd06e4d2a97488255a
Ubuntu <ubuntu@ip-10-48-45-105.eu-west-1.compute.internal>
2013-07-16 13:04:43 +0000

    handle file upload

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,8 @@
 
 
 var exec = require("child_process").exec;
-var querystring = require("querystring");
 var fs = require("fs");
+var formidable = require("formidable"); 
 
 function start(response) {
   console.log("Request handler 'starter' was called.");  
@@ -25,12 +25,24 @@ function start(response) {
   response.end();
 }
 
-function upload(response, postData){
-  console.log("Request handler 'upload' was called.");  
+function upload(response, request){
+  console.log("Request handler 'upload' was called.");
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(error, fields, files) {
+    console.log("parsing done");
+    
+    fs.rename(files.upload.path, "tmp/test.png", function(error){
+      if(error){
+        fs.unlink("tmp/test.png");
+        fs.rename(files.upload.path, "tmp/test.png");
+      }
+    });
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You have sent the text:" + 
-   querystring.parse(postData).text);
+  response.write("received image:<br/>");
+  response.write("<img src='/show' />");
   response.end();
+  });
  }
 
 
--- server.js
+++ server.js
@@ -10,22 +10,9 @@ var formidable
 function start(route, handle) {
   var port = process.env.PORT || 5000;
   http.createServer(function(request,response) {
-    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request received for"+ pathname + " received." ); 
-   
-    request.setEncoding("utf8");
-   
-    request.addListener("data", function(postDataChunk){
-      postData += postDataChunk;
-      console.log("Received POST data chunk '" +
-       postDataChunk + "'.");
-    });
-    
-    request.addListener("end", function(){
-      route(handle, pathname, response, postData);
-    });
-
+    route(handle, pathname, response, postData);
   }).listen(port);
   console.log("Server has started");
 }
